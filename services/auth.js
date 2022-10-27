f095060dd01bb39480fea610353f51062f089eb5
renukajalluri <renukajalluri20@gmail.com>
2022-10-27 18:54:32 +0530

    commit

--- services/auth.js
+++ services/auth.js
@@ -1,16 +1,16 @@
-import axios from 'axios'
-import config from '../config'
+import axios from "axios";
+import config from "../config";
 // const baseUrl = `${config.getServerHost()}/auth`
-const baseUrl = "https://jira-be.herokuapp.com/auth/"
+const baseUrl = "https://jira-be.herokuapp.com/auth/";
 
-const login = async credentials => {
-    const response = await axios.post(`${baseUrl}/login`, credentials)
-    return response.data
-  }
+const login = async (credentials) => {
+  const response = await axios.post(`${baseUrl}/login`, credentials);
+  return response.data;
+};
 
-  const signup = async credentials => {
-    const response = await axios.post(`${baseUrl}/sign-up`, credentials)
-    return response.data
-  }
-  
-export default { login,signup }
+const signup = async (credentials) => {
+  const response = await axios.post(`${baseUrl}/sign-up`, credentials);
+  return response.data;
+};
+
+export default { login, signup };
--- services/comment.js
+++ services/comment.js
@@ -1,17 +1,17 @@
-import axios from 'axios'
-import config from '../config'
-const baseUrl = "https://jira-be.herokuapp.com/comment/"
+import axios from "axios";
+import config from "../config";
+const baseUrl = "https://jira-be.herokuapp.com/comment/";
 // const baseUrl = `${config.getServerHost()}/comment`
 
-const addComment = async (dataObj,header) => {
-    console.log(dataObj,header)
-    const response = await axios.post(baseUrl, dataObj,header)
-    return response.data
-  }
+const addComment = async (dataObj, header) => {
+  console.log(dataObj, header);
+  const response = await axios.post(baseUrl, dataObj, header);
+  return response.data;
+};
 
-  const getComments = async (issueId,header) => {
-    const response = await axios.get(`${baseUrl}/${issueId}`,header)
-    return response.data
-  }
+const getComments = async (issueId, header) => {
+  const response = await axios.get(`${baseUrl}/${issueId}`, header);
+  return response.data;
+};
 
-export default  {getComments,addComment}
+export default { getComments, addComment };
